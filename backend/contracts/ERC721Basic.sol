1789ba79aedee7eda1d01e651e36f5af273f709d
django_freeman <o.tampodo.t.18@gmail.com>
2022-04-10 18:53:33 +0900

    綴り間違いを修正

--- backend/contracts/ERC721Basic.sol
+++ backend/contracts/ERC721Basic.sol
@@ -8,25 +8,25 @@ pragma solidity ^0.5.16;
 //ERC721トークンのInterface
 contract ERC721Basic {
     //Transferのイベント発行
-    event Transfer(address indexed _from, address indexed _to, unit256 _tokenId);
+    event Transfer(address indexed _from, address indexed _to, uint256 _tokenId);
     
     //Approvalのイベント発行
-    event Approval(address indexed _owner, address indexed _approved, unit256 _tokenId);
+    event Approval(address indexed _owner, address indexed _approved, uint256 _tokenId);
     
     //ApprovalForAllのイベント発行
     event ApprovalForAll(address indexed _owner, address indexed operator, bool _approved);
 
     //残高を調べる
-    function balanceOf(address _owner) public view returns (unit256 _balance);
+    function balanceOf(address _owner) public view returns (uint256 _balance);
     //owenerを調べる
-    function ownerOf(unit256 _tokenId) public view returns (address _owner);
+    function ownerOf(uint256 _tokenId) public view returns (address _owner);
     //存在を確かめる
-    function exists(unit256 _tokenid) public view returns (bool _exists);
+    function exists(uint256 _tokenid) public view returns (bool _exists);
 
     //承認
-    function approve(address _to, unit256 _tokenId) public view;
+    function approve(address _to, uint256 _tokenId) public view;
     //承認を得る
-    function getApproved(unit256 _tokenid) public view returns (address _operator);
+    function getApproved(uint256 _tokenid) public view returns (address _operator);
 
     //全員に承認を与える
     function setApprovalForAll(address _operator, bool _approved) public;
@@ -34,14 +34,14 @@ contract ERC721Basic {
     function isApprovedForAll(address _owner, address _operator) public view returns (bool);
 
     //資金移転
-    function transferFrom(address _from, address _to, unit256 _tokenId) public;
+    function transferFrom(address _from, address _to, uint256 _tokenId) public;
     //安全な資金移転
-    function safeTransferFrom(address _from, address _to, unit256 _tokenId) public;
+    function safeTransferFrom(address _from, address _to, uint256 _tokenId) public;
     //禁錮の資金移転
     function safeTransferFrom(
         address _from,
         address _to,
-        unit256 _tokenId,
+        uint256 _tokenId,
         bytes memory _data
     )
      public;
