9e0f0833e2135e6ea4c1dcf0bd6e57a64e54a0a7
django_freeman <o.tampodo.t.18@gmail.com>
2022-04-11 19:08:34 +0900

    ERC721のホールダーを追加

---
+++ backend/contracts/ERC721Holder.sol
@@ -0,0 +1,9 @@
+pragma solidity ^0.5.16;
+
+import "./ERC721Receiver";
+
+contract ERC721Holder is ERC721Receiver {
+    function onERC721Received(address, uint256, bytes memory) public returns(bytes4) {
+        return ERC721_RECEIVED;        
+    }
+}
