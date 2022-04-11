dac217f74edafb6a1116e12274e37f3843431662
django_freeman <o.tampodo.t.18@gmail.com>
2022-04-11 19:03:14 +0900

    ERC721のレシーバインターフェースを追加

---
+++ backend/contracts/ERC721Receiver.sol
@@ -0,0 +1,13 @@
+pragma solidity ^0.5.16;
+
+//ERC721のトークンレシーバインターフェース
+//operatorの引数は省略
+contract ERC721Receiver {
+    bytes4 constant ERC721_RECEIVED = 0xf0b9e5ba;
+
+    function onERC721Received(
+        address _from,
+        uint256 _tokenId,
+        bytes memory _data
+    ) public returns(bytes4);
+}
