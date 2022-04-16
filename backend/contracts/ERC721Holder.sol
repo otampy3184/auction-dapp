e0fc5b837564ecafe5ac7c593a45defe83e4c4ef
django_freeman <o.tampodo.t.18@gmail.com>
2022-04-16 13:55:42 +0900

    minor change

--- backend/contracts/ERC721Holder.sol
+++ backend/contracts/ERC721Holder.sol
@@ -5,7 +5,7 @@ pragma solidity ^0.8.0;
 import "./ERC721Receiver.sol";
 
 contract ERC721Holder is ERC721Receiver {
-    function onERC721Received(address, uint256, bytes memory) public returns(bytes4) {
+    function onERC721Received(address, uint256, bytes memory) public override pure returns(bytes4) {
         return ERC721_RECEIVED;        
     }
 }
